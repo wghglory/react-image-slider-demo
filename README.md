f6d3c9f282836beebf4b9ac2d8009c69516884c1
Guanghui Wang <wghglory89@gmail.com>
2017-11-05 21:02:46 +0800

    update images' path

--- README.md
+++ README.md
@@ -1,22 +1,8 @@
-# React Boilerplate
+# React Image Slider Demo
 
 ## Running Project
 
 ```bash
 npm install
 npm start
-```
-
-## Support ES7 `async await`
-
-webpack config `babel-polyfill`
-
-```javascript
-entry: ['babel-polyfill', './src/index.js'],
-```
-
-## Webpack support Debug
-
-```javascript
-devtool: 'source-map',
 ```
--- src/components/App.js
+++ src/components/App.js
@@ -43,24 +43,24 @@ export default class App extends Component {
     return (
       <Frame>
         <Carousel showIndex={this.state.showIndex} nav={this.renderNav()} width={640}>
-          <Slide image={require('./images/1.jpg')} title="Imperial Mockery">
+          <Slide image={require('../assets/img/1.jpg')} title="Imperial Mockery">
             In a show of defiance, rebels have again made mockery of the majesty that is service to the Empire. These
             objects were immediately removed from the reflecting pool in Coruscant's Central Square when found this
             morning.
           </Slide>
-          <Slide image={require('./images/2.jpg')} title="Trooper Initiation">
+          <Slide image={require('../assets/img/2.jpg')} title="Trooper Initiation">
             In a tradition that predates the Empire, Troopers seen here are made to suffer the agony of the limbo line.
             Such initiation techniques are currently under Imperial review.
           </Slide>
-          <Slide image={require('./images/3.jpg')} title="Master and Apprentice">
+          <Slide image={require('../assets/img/3.jpg')} title="Master and Apprentice">
             Internships work! When determining a career path, it is wise to first find someone currently in that line of
             work. They will likely be able to show you what really like before you commit your future.
           </Slide>
-          <Slide image={require('./images/4.jpg')} title="Battle of Coruscant">
+          <Slide image={require('../assets/img/4.jpg')} title="Battle of Coruscant">
             Modelers ran out of the pre-Empire model Star Destroyers in this reenactment of the Battle of Coruscant.
             Various plans for future machines of war were used as filler.
           </Slide>
-          <Slide image={require('./images/5.jpg')} title="The Wiles of the Forest">
+          <Slide image={require('../assets/img/5.jpg')} title="The Wiles of the Forest">
             After a number of nervous breakdowns, Empire personnel officers are investigating the possibility rotating
             troopers stationed on the forest moons of Endor after several rotations.
           </Slide>
